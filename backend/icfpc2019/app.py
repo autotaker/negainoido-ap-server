ad1c1d58a2ba11ddf6289444049709cdd8bacc4e
autotaker <autotaker@gmail.com>
2020-01-13 19:11:06 +0900

    fetch problem

--- backend/icfpc2019/app.py
+++ backend/icfpc2019/app.py
@@ -1,7 +1,49 @@
 from flask import Flask, jsonify
+import os
+import mysql.connector as db
+
+dbconfig = {
+    "database": os.getenv('DB_NAME') or 'icfpc2019',
+    "user": os.getenv('DB_USER') or 'negainoido',
+    "password": os.getenv('DB_PASS') or 'negainoido',
+    "charset": 'utf8mb4',
+    "collation": 'utf8mb4_bin',
+    "pool_name": 'pool-icfpc2019'
+}
+
+if os.getenv('PRODUCTION'):
+    dbconfig['unix_socket'] = '/cloudsql/negainoido-icfpc-platform:asia-northeast1:mysql-negainoido'
+else:
+    dbconfig['host'] = os.getenv('DB_HOST') or 'localhost'
+    dbconfig['port'] = os.getenv('DB_PORT') or '3306'
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return jsonify({"message": "hello world icfpc2019"})
+
+@app.route('/problems')
+def problems():
+    conn = db.connect(**dbconfig)
+    try:
+        cur = conn.cursor()
+        try: 
+            query = '''
+                SELECT `name`, `description`, `url` FROM problem
+                ORDER BY `name` ASC
+                LIMIT 20
+            '''
+            cur.execute(query)
+            result = []
+            for row in cur.fetchall():
+                obj = {}
+                obj["name"] = row[0]
+                obj["description"] = row[1]
+                obj["url"] = row[2]
+                result.append(obj)
+            return jsonify(result)
+        finally:
+            cur.close()
+    finally:
+        conn.close()
--- backend/main.py
+++ backend/main.py
@@ -8,20 +8,7 @@ import os
 
 app = Flask(__name__)
 
-dbconfig = {
-    "database": os.getenv('DB_NAME') or 'negainoido',
-    "user": os.getenv('DB_USER') or 'negainoido',
-    "password": os.getenv('DB_PASS') or 'negainoido',
-    "charset": 'utf8mb4',
-    "collation": 'utf8mb4_bin',
-    "pool_name": 'dbpool'
-}
 
-if os.getenv('DB_SOCK'):
-    dbconfig['unix_socket'] = os.getenv('DB_SOCK')
-else:
-    dbconfig['host'] = os.getenv('DB_HOST') or 'localhost'
-    dbconfig['port'] = os.getenv('DB_PORT') or '3306'
 
 @app.route('/api/v1/hello')
 def index():
