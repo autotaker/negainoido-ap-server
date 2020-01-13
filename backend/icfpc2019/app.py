2fe09aeceaa3ae7d918697636b298feb3e9e24a1
autotaker <autotaker@gmail.com>
2020-01-14 08:59:42 +0900

    implement paging

--- backend/icfpc2019/app.py
+++ backend/icfpc2019/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 import mysql.connector as db
 
@@ -23,17 +23,35 @@ app = Flask(__name__)
 def index():
     return jsonify({"message": "hello world icfpc2019"})
 
+def get_problem_count(cur):
+    query = '''
+        SELECT COUNT(*) FROM problem
+    '''
+    cur.execute(query)
+    row = cur.fetchone()
+    return row[0]
+
+def get_page():
+    pageParam = request.args.get('page')
+    if pageParam:
+        return int(pageParam)
+    else:
+        return 1
+
 @app.route('/problems')
 def problems():
     conn = db.connect(**dbconfig)
+    page = get_page()
+    page_size = 20
+    page_offset = (page - 1) * page_size
     try:
         cur = conn.cursor()
         try: 
             query = '''
                 SELECT `name`, `description`, `url` FROM problem
                 ORDER BY `name` ASC
-                LIMIT 20
-            '''
+                LIMIT {} OFFSET {}
+            '''.format(page_size, page_offset)
             cur.execute(query)
             result = []
             for row in cur.fetchall():
@@ -42,7 +60,11 @@ def problems():
                 obj["description"] = row[1]
                 obj["url"] = row[2]
                 result.append(obj)
-            return jsonify(result)
+            response = jsonify(result)
+            response.headers['X-Page-Size'] = 20
+            response.headers['X-Page'] = page
+            response.headers['X-Total'] = get_problem_count(cur) 
+            return response
         finally:
             cur.close()
     finally:
